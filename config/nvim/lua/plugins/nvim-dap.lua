67ab3c39cb9686a38bcbd3e846ad875b3f57411e
Fallenwood <fallenwood.y@outlook.com>
2025-03-30 08:33:02 +0800

    DAP

--- config/nvim/lua/plugins/nvim-dap.lua
+++ config/nvim/lua/plugins/nvim-dap.lua
@@ -1,9 +1,66 @@
+local load = require
+
 local module = {
-  -- {
-  --   "mfussenegger/nvim-dap",
-  --   config = function()
-  --   end
-  -- },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+      "nicholasmata/nvim-dap-cs",
+    },
+    config = function()
+      local dap = load("dap")
+      local dapui = load("dapui")
+      dap.adapters.codelldb = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = "/home/vbox/.vscode-server/extensions/vadimcn.vscode-lldb-1.11.4/adapter/codelldb",
+          args = { "--port", "${port}" },
+        }
+      }
+      dap.configurations.zig = {
+        {
+          name = "Launch",
+          type = "codelldb",
+          request = "launch",
+          program = "${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}",
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+          args = {},
+        }
+      }
+      load("dap-cs").setup({
+        netcoredbg = {
+          path = "/home/vbox/.local/opt/netcoredbg/netcoredbg",
+        }
+      })
+
+      dapui.setup()
+
+      -- vim.keymap.set("n", "<leader>duo", dapui.open, {})
+      -- vim.keymap.set("n", "<leader>duc", dapui.close, {})
+      vim.keymap.set("n", "<leader>dt", dapui.toggle, {})
+
+      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<leader>dc", dap.continue, {})
+      -- vim.keymap.set("n", "<leader>dso", dap.step_over, {})
+      -- vim.keymap.set("n", "<leader>dsi", dap.step_into, {})
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end
+  },
 }
 
 return module
