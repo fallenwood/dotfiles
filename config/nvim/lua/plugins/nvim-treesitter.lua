500c7ed46937643e7bd1d384a94861788b674f6e
Fallenwood <fallenwood.y@outlook.com>
2026-03-31 16:00:18 +0800

    Remove F# treesitter plugin configuration

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -32,39 +32,6 @@ local module = {
     lazy = false,
     enabled = true,
   },
-
-  {
-    "https://github.com/Nsidorenco/tree-sitter-fsharp",
-    branch = "develop",
-    dependencies = {
-      "https://github.com/nvim-treesitter/nvim-treesitter",
-    },
-    build = function()
-      vim.fn.system({
-        "npm",
-        "install",
-        "&&",
-        "npm",
-        "run",
-        "build",
-      })
-    end,
-    config = function()
-      local parser_config = load("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.fsharp = {
-        install_info = {
-          url = vim.fn.stdpath("data") .. "/lazy/tree-sitter-fsharp",
-          files = { "src/scanner.cc", "src/parser.c", },
-        },
-        filetype = "fsharp",
-      }
-      -- Needs to install tree-sitter binary
-      -- Run `CC=clang++ nvim` and `:TSInstallForGrammar fsharp` to install
-      -- No fsharp syntax bundled with neovim, no coloring at all
-    end,
-    lazy = true,
-    enabled = false,
-  },
 }
 
 return module
