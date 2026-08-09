328747ae9126ee7f2d3a076bb3e16bd2e17493fc
Fallenwood <fallenwood.y@outlook.com>
2026-08-09 08:43:11 +0800

    Update

--- toolbox/init.sh
+++ toolbox/init.sh
@@ -1,7 +1,10 @@
+sudo mkdir -p /home/linuxbrew/
+sudo ln -s $HOME/.local/share/zerobrew/prefix /home/linuxbrew/.linuxbrew
 sudo dnf update -y \
-  && sudo dnf install neovim \
-     gcc clang cmake mold sccache \
-     rustup \
-     lldb \
-     dotnet-sdk-8.0 \
-     dotnet-sdk-9.0
+  && sudo dnf install \
+     gcc clang cmake sccache lldb \ # C++ / rust
+     gtk4-devel \ # ghostty
+     gtk4-layer-shell-devel \
+     libadwaita-devel \
+     gettext \
+     blueprint-compiler
